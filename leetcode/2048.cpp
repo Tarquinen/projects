206aeb0435b881ddb80ce38ff5e3460a7b799006
dannysmo@gmail.com <dannysmo@gmail.com>
2024-05-28 15:51:40 +0200

    2048 working

--- leetcode/2048.cpp
+++ leetcode/2048.cpp
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <iomanip>
 #include <random>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
@@ -35,20 +37,11 @@ class board {
          return result;
       }
 
-   public:
-      board() {
-         for (int i = 0; i < 4; i ++) {
-            for (int j = 0; j < 4; j++) {
-               boardGrid[i][j] = 0;
-            }
-         }
-         generateRandomCell();
-         generateRandomCell();
-      }
-
-      friend ostream& operator<<(ostream& os, const board& b) {
-         os << b.toString();
-         return os;
+      int generateRandomNumber(int x) {
+         std::random_device rd;
+         std::mt19937 gen(rd());
+         std::uniform_int_distribution<> dis(1, x);
+         return dis(gen);
       }
 
       int countBlankCells() const {
@@ -63,18 +56,42 @@ class board {
          return count;
       }
 
-      int generateRandomNumber(int x) {
-         std::random_device rd;
-         std::mt19937 gen(rd());
-         std::uniform_int_distribution<> dis(1, x);
-         return dis(gen);
+   public:
+      board() {
+         for (int i = 0; i < 4; i ++) {
+            for (int j = 0; j < 4; j++) {
+               boardGrid[i][j] = 0;
+            }
+         }
+         // generaterandomcell();
+         // generaterandomcell();
+         setCellValue(0, 0, 2);
+         setCellValue(0, 1, 2);
+
+         setCellValue(1, 1, 2);
+         setCellValue(1, 2, 2);
+
+         setCellValue(2, 0, 2);
+         setCellValue(2, 1, 2);
+         setCellValue(2, 2, 2);
+         setCellValue(2, 3, 2);
+
+         
+         setCellValue(3, 0, 2);
+         setCellValue(3, 1, 2);
+         setCellValue(3, 2, 4);
+      }
+
+      friend ostream& operator<<(ostream& os, const board& b) {
+         os << b.toString();
+         return os;
       }
 
       bool generateRandomCell() {
          int possiblePositions = countBlankCells();
          if (possiblePositions == 0) return false;
          int newCell = generateRandomNumber(possiblePositions);
-         cout << "new cell at: " << newCell << endl;
+         // cout << "new cell at: " << newCell << endl;
          int currCell = 0;
          for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
@@ -94,6 +111,154 @@ class board {
          return true;
       }
 
+      void pushLeft() {
+         bool modified[4][4] { false };
+         for (int i = 0; i < 4; i++) {
+            // move cells over
+            for (int j = 1; j < 4; j++) {
+               int currentCell = boardGrid[i][j];
+               if (currentCell != 0) {
+                  if (boardGrid[i][j-1] == 0) {
+                     boardGrid[i][j] = 0;
+                     boardGrid[i][j-1] = currentCell;
+                     if (modified[i][j]) {
+                        modified[i][j] = false;
+                        modified[i][j-1] = true;
+                     }
+                  }
+                  if (j > 1 && getCellValue(i, j-2) == 0) {
+                     j-=2;
+                  }
+               }
+            }
+            // combine cells if possible
+            for (int j = 1; j < 4; j++) {
+               int currentCell = boardGrid[i][j];
+               if (currentCell != 0) {
+                  if (boardGrid[i][j-1] == currentCell && !modified[i][j] && !modified[i][j-1]) {
+                     boardGrid[i][j-1] = currentCell * 2;
+                     boardGrid[i][j] = 0;
+                     modified[i][j-1] = true;
+                     modified[i][j] = false;
+                     i--;
+                     break;
+                  }
+               }
+            }
+         }
+      }
+
+      void pushRight() {
+         bool modified[4][4] { false };
+         for (int i = 0; i < 4; i++) {
+            // move cells over
+            for (int j = 2; j >= 0; j--) {
+               int currentCell = boardGrid[i][j];
+               if (currentCell != 0) {
+                  if (boardGrid[i][j+1] == 0) {
+                     boardGrid[i][j] = 0;
+                     boardGrid[i][j+1] = currentCell;
+                     if (modified[i][j]) {
+                        modified[i][j] = false;
+                        modified[i][j+1] = true;
+                     }
+                  }
+                  if (j < 2 && getCellValue(i, j+2) == 0) {
+                     j+=2;
+                  }
+               }
+            }
+            // combine cells if possible
+            for (int j = 2; j >= 0; j--) {
+               int currentCell = boardGrid[i][j];
+               if (currentCell != 0) {
+                  if (boardGrid[i][j+1] == currentCell && !modified[i][j] && !modified[i][j+1]) {
+                     boardGrid[i][j+1] = currentCell * 2;
+                     boardGrid[i][j] = 0;
+                     modified[i][j+1] = true;
+                     modified[i][j] = false;
+                     i--;
+                     break;
+                  }
+               }
+            }
+         }
+      }
+
+      void pushUp() {
+         bool modified[4][4] { false };
+         for (int j = 0; j < 4; j++) {
+            // move cells over
+            for (int i = 1; i < 4; i++) {
+               int currentCell = boardGrid[i][j];
+               if (currentCell != 0) {
+                  if (boardGrid[i-1][j] == 0) {
+                     boardGrid[i][j] = 0;
+                     boardGrid[i-1][j] = currentCell;
+                     if (modified[i][j]) {
+                        modified[i][j] = false;
+                        modified[i-1][j] = true;
+                     }
+                  }
+                  if (i > 1 && getCellValue(i-2, j) == 0) {
+                     i-=2;
+                  }
+               }
+            }
+            // combine cells if possible
+            for (int i = 1; i < 4; i++) {
+               int currentCell = boardGrid[i][j];
+               if (currentCell != 0) {
+                  if (boardGrid[i-1][j] == currentCell && !modified[i][j] && !modified[i-1][j]) {
+                     boardGrid[i-1][j] = currentCell * 2;
+                     boardGrid[i][j] = 0;
+                     modified[i-1][j] = true;
+                     modified[i][j] = false;
+                     j--;
+                     break;
+                  }
+               }
+            }
+         }
+      }
+
+      void pushDown() {
+         bool modified[4][4] { false };
+         for (int j = 0; j < 4; j++) {
+            // move cells over
+            for (int i = 2; i >= 0; i--) {
+               int currentCell = boardGrid[i][j];
+               if (currentCell != 0) {
+                  if (boardGrid[i+1][j] == 0) {
+                     boardGrid[i][j] = 0;
+                     boardGrid[i+1][j] = currentCell;
+                     if (modified[i][j]) {
+                        modified[i][j] = false;
+                        modified[i+1][j] = true;
+                     }
+                  }
+                  if (i < 2 && getCellValue(i+2, j) == 0) {
+                     i+=2;
+                  }
+               }
+            }
+            // combine cells if possible
+            for (int i = 2; i >= 0; i--) {
+               int currentCell = boardGrid[i][j];
+               if (currentCell != 0) {
+                  if (boardGrid[i+1][j] == currentCell && !modified[i][j] && !modified[i+1][j]) {
+                     boardGrid[i+1][j] = currentCell * 2;
+                     boardGrid[i][j] = 0;
+                     modified[i+1][j] = true;
+                     modified[i][j] = false;
+                     j--;
+                     break;
+                  }
+               }
+            }
+         }
+      }
+
       void setCellValue(int row, int col, int value) {
          boardGrid[row][col] = value;
       }
@@ -103,9 +268,36 @@ class board {
       }
 };
 
+void clearScreen() {
+   cout << "\033[2J\033[1;1H";
+}
 
 int main() {
+   clearScreen();
    board b;
    cout << b << endl;
-   return 0;
+   // std::this_thread::sleep_for(std::chrono::seconds(1));
+   
+   while (true) {
+      string move;
+      cin >> move;
+      if (move == "w") {
+         clearScreen();
+         b.pushUp();
+      }
+      else if (move == "s") {
+         clearScreen();
+         b.pushDown();
+      }
+      else if (move == "d") {
+         clearScreen();
+         b.pushRight();
+      }
+      else if (move == "a") {
+         clearScreen();
+         b.pushLeft();
+      }
+      b.generateRandomCell();
+      cout << b << endl;
+   }
 }
