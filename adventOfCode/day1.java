3a382f189c5034831146120f315ac2efeeae8559
Tarquinen <dannysmo@gmail.com>
2024-07-18 08:32:00 +0200

    format some files

--- adventOfCode/day1.java
+++ adventOfCode/day1.java
@@ -1,19 +1,20 @@
 //https://adventofcode.com/2023/day/1
 
 package adventOfCode;
+
 import java.io.*;
 import java.util.ArrayList;
 
 public class day1 {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader file = new BufferedReader(new FileReader(
-            "C:\\Users\\danny\\OneDrive\\Desktop\\Algo\\School-repo\\adventOfCode\\day1.txt"));
+                "C:\\Users\\danny\\OneDrive\\Desktop\\Algo\\School-repo\\adventOfCode\\day1.txt"));
 
         part1(file);
-        //part2(file);
+        // part2(file);
     }
 
-    public static void part1 (BufferedReader file) throws Exception {
+    public static void part1(BufferedReader file) throws Exception {
         int i;
         int line = 0;
         int totalSum = 0;
@@ -21,10 +22,10 @@ public class day1 {
 
         while ((i = file.read()) != -1) {
             if (i > 47 && i < 58) {
-                str += (char)i;
+                str += (char) i;
             }
             if (i == 13) {
-                line ++;
+                line++;
                 if (str.length() > 1)
                     str = "" + str.charAt(0) + str.charAt(str.length() - 1);
                 else if (str.length() == 1)
@@ -37,47 +38,57 @@ public class day1 {
 
         System.out.println("total sum is: " + totalSum);
     }
-    
-    public static void part2 (BufferedReader file) throws Exception {
+
+    public static void part2(BufferedReader file) throws Exception {
         String line = null;
-        String[] dict = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] dict = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
         String numString = "";
         ArrayList<String> fileList = new ArrayList<String>();
         ArrayList<String> numList = new ArrayList<String>();
-        while ((line = file.readLine()) != null) { //create a list of strings where each string is a row in the file
-                fileList.add(line);
+        while ((line = file.readLine()) != null) { // create a list of strings where each string is a row in the file
+            fileList.add(line);
         }
 
         for (int rows = 0; rows < fileList.size(); rows++) { // loop through each row
-            for (int chars = 0; chars < fileList.get(rows).length(); chars++) { //loop through each char in the string
-                if (Character.isDigit(fileList.get(rows).charAt(chars))) { //add numbers into numList
+            for (int chars = 0; chars < fileList.get(rows).length(); chars++) { // loop through each char in the string
+                if (Character.isDigit(fileList.get(rows).charAt(chars))) { // add numbers into numList
                     numString += fileList.get(rows).charAt(chars);
                     continue;
                 }
                 boolean wordFound = false;
-                for (int dictIndex = 0; dictIndex < dict.length; dictIndex ++) { //check the char against dict, if it matches check more following chars
+                for (int dictIndex = 0; dictIndex < dict.length; dictIndex++) { // check the char against dict, if it
+                                                                                // matches check more following chars
                     if (wordFound)
                         break;
                     int wordLength = dict[dictIndex].length();
                     int matchCounter = 0;
                     for (int extraLetters = 0; extraLetters < wordLength; extraLetters++) {
-                        if (chars + extraLetters >= fileList.get(rows).length()) // stop checking for matches when you get to the end of a row
+                        if (chars + extraLetters >= fileList.get(rows).length()) // stop checking for matches when you
+                                                                                 // get to the end of a row
                             break;
-                        if (fileList.get(rows).charAt(chars + extraLetters) != dict[dictIndex].charAt(extraLetters)) {//letter doesn't match so check against next dict item
+                        if (fileList.get(rows).charAt(chars + extraLetters) != dict[dictIndex].charAt(extraLetters)) {// letter
+                                                                                                                      // doesn't
+                                                                                                                      // match
+                                                                                                                      // so
+                                                                                                                      // check
+                                                                                                                      // against
+                                                                                                                      // next
+                                                                                                                      // dict
+                                                                                                                      // item
                             break;
-                        }
-                        else if (fileList.get(rows).charAt(chars + extraLetters) == dict[dictIndex].charAt(extraLetters)) {//letters match a dictionary element
-                            matchCounter ++;
-                            if (matchCounter == wordLength) {//word matches
+                        } else if (fileList.get(rows).charAt(chars + extraLetters) == dict[dictIndex]
+                                .charAt(extraLetters)) {// letters match a dictionary element
+                            matchCounter++;
+                            if (matchCounter == wordLength) {// word matches
                                 // add number to numString
                                 numString += dictIndex;
                                 wordFound = true;
-                                //advance cursor forward l places to be efficient maybe later
-                                chars += wordLength - 2; // this is really wordLength - 1 because each iteration is +1 as well
+                                // advance cursor forward l places to be efficient maybe later
+                                chars += wordLength - 2; // this is really wordLength - 1 because each iteration is +1
+                                                         // as well
                                 break;
                             }
-                        }
-                        else {
+                        } else {
                             break;
                         }
 
@@ -96,7 +107,7 @@ public class day1 {
             String strTotal = "" + value.charAt(0) + value.charAt(value.length() - 1);
             int rowTotal = Integer.parseInt(strTotal);
             total += rowTotal;
-            rowCounter ++; 
+            rowCounter++;
             System.out.println("row: " + rowCounter + " number: " + rowTotal + " (" + value + ")");
         }
 
--- leetcode/100prisoners.cpp
+++ leetcode/100prisoners.cpp
@@ -1,106 +1,103 @@
 // https://rosettacode.org/wiki/100_prisoners
-#include <iostream>
-#include <chrono>
-#include <random>
 #include <algorithm>
 #include <cassert>
+#include <chrono>
+#include <iostream>
+#include <random>
 
 using namespace std;
 
-int* shuffledArray(int length, int minValue, int maxValue)
-{
-   assert(maxValue - minValue + 1 >= length && "Range is too small for the requested array length without duplicates.");
+int *shuffledArray(int length, int minValue, int maxValue) {
+  assert(
+      maxValue - minValue + 1 >= length &&
+      "Range is too small for the requested array length without duplicates.");
 
-   int range = maxValue - minValue + 1;
-   int * tempArray = new int[range];
-   for (int i = 0; i < range; i++)
-   {
-      tempArray[i] = minValue + i;
-   }
+  int range = maxValue - minValue + 1;
+  int *tempArray = new int[range];
+  for (int i = 0; i < range; i++) {
+    tempArray[i] = minValue + i;
+  }
 
-   unsigned seed = chrono::system_clock::now().time_since_epoch().count();
-   shuffle(tempArray, tempArray + range, default_random_engine(seed));
+  unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+  shuffle(tempArray, tempArray + range, default_random_engine(seed));
 
-   int* array = new int[length];
-   for (int i = 0; i < length; ++i) {
-      array[i] = tempArray[i];
-   }
+  int *array = new int[length];
+  for (int i = 0; i < length; ++i) {
+    array[i] = tempArray[i];
+  }
 
-   delete[] tempArray;
-   return array;
+  delete[] tempArray;
+  return array;
 }
 
 int noStrategy() {
-   int* drawers;
-   int* listOfDrawers;
-   int freedPrisoners { 0 };
+  int *drawers;
+  int *listOfDrawers;
+  int freedPrisoners{0};
 
-   drawers = shuffledArray(100, 1, 100);
+  drawers = shuffledArray(100, 1, 100);
 
-   for (int prisoner = 0; prisoner < 100; prisoner++)
-   {
-      listOfDrawers = shuffledArray(50, 1, 100);
-      for (int attempt = 0; attempt < 50; attempt++)
-      {
-         if (drawers[listOfDrawers[attempt] - 1] == prisoner + 1)
-         {
-            freedPrisoners++;
-            break;
-         }
+  for (int prisoner = 0; prisoner < 100; prisoner++) {
+    listOfDrawers = shuffledArray(50, 1, 100);
+    for (int attempt = 0; attempt < 50; attempt++) {
+      if (drawers[listOfDrawers[attempt] - 1] == prisoner + 1) {
+        freedPrisoners++;
+        break;
       }
-      delete[] listOfDrawers;
-   }
-   delete[] drawers;
-   return freedPrisoners;
+    }
+    delete[] listOfDrawers;
+  }
+  delete[] drawers;
+  return freedPrisoners;
 }
 
 int bestStrategy() {
-   int* drawers;
-   int freedPrisoners { 0 };
+  int *drawers;
+  int freedPrisoners{0};
 
-   drawers = shuffledArray(100, 1, 100);
+  drawers = shuffledArray(100, 1, 100);
 
-   for (int prisoner = 0; prisoner < 100; prisoner++) {
-      int drawerToCheck = prisoner;
-      for (int attempt = 0; attempt < 50; attempt++) {
-         if (drawers[drawerToCheck] == prisoner + 1) {
-            freedPrisoners++;
-            // cout << " attempt: " << attempt + 1 << " checking drawer " << drawerToCheck << " for prisoner " << prisoner << " found " << drawers[drawerToCheck] << "\n";
-            break;
-         }
-         else {
-            // cout << " attempt: " << attempt  + 1 << " checking drawer " << drawerToCheck << " for prisoner " << prisoner << " found " << drawers[drawerToCheck] << "\n";
-            drawerToCheck = drawers[drawerToCheck] - 1;
-         }
+  for (int prisoner = 0; prisoner < 100; prisoner++) {
+    int drawerToCheck = prisoner;
+    for (int attempt = 0; attempt < 50; attempt++) {
+      if (drawers[drawerToCheck] == prisoner + 1) {
+        freedPrisoners++;
+        // cout << " attempt: " << attempt + 1 << " checking drawer " <<
+        // drawerToCheck << " for prisoner " << prisoner << " found " <<
+        // drawers[drawerToCheck] << "\n";
+        break;
+      } else {
+        // cout << " attempt: " << attempt  + 1 << " checking drawer " <<
+        // drawerToCheck << " for prisoner " << prisoner << " found " <<
+        // drawers[drawerToCheck] << "\n";
+        drawerToCheck = drawers[drawerToCheck] - 1;
       }
-   }
-   delete[] drawers;
-   return freedPrisoners;
+    }
+  }
+  delete[] drawers;
+  return freedPrisoners;
 }
 
+int main() {
+  int successfulSimulations{0};
+  int simulations{100000};
+  auto start = chrono::high_resolution_clock::now();
+  auto end = chrono::high_resolution_clock::now();
+  auto duration =
+      chrono::duration_cast<chrono::milliseconds>(end - start).count();
 
+  cout << successfulSimulations << "/" << simulations
+       << " successful simulations, took " << duration << " ms\n";
 
-int main()
-{
-   int successfulSimulations { 0 };
-   int simulations { 100000 };
-   auto start = chrono::high_resolution_clock::now();
-   auto end = chrono::high_resolution_clock::now();
-   auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
-
-   cout << successfulSimulations << "/" << simulations << " successful simulations, took " << duration << " ms\n";
-
-   successfulSimulations = 0;
-   start = chrono::high_resolution_clock::now();
-   for (int i = 0; i < simulations; i++)
-   {
-      if (bestStrategy() == 100)
-      {
-         successfulSimulations++;
-      }
-   }
-   end = chrono::high_resolution_clock::now();
-   duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
-   cout << successfulSimulations << "/" << simulations << " successful simulations, took " << duration << " ms\n";
-
+  successfulSimulations = 0;
+  start = chrono::high_resolution_clock::now();
+  for (int i = 0; i < simulations; i++) {
+    if (bestStrategy() == 100) {
+      successfulSimulations++;
+    }
+  }
+  end = chrono::high_resolution_clock::now();
+  duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+  cout << successfulSimulations << "/" << simulations
+       << " successful simulations, took " << duration << " ms\n";
 }
